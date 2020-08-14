1a7c4592133a905da27b7b718fefa09bbb2be04a
loki <loki@macosdembp.home>
2020-08-14 16:39:50 +1000

    Updates

---
+++ precache-manifest.a0ca6557056c92fd2dc2c82dd916aa87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "223ce5443e5efe41c1a4b972db99ba9c",
+    "url": "/react-admin/index.html"
+  },
+  {
+    "revision": "9fe7fe61886730c56c45",
+    "url": "/react-admin/static/css/2.e6fc7d0e.chunk.css"
+  },
+  {
+    "revision": "d6fa8f48258aff8525d1",
+    "url": "/react-admin/static/css/main.871cb891.chunk.css"
+  },
+  {
+    "revision": "9fe7fe61886730c56c45",
+    "url": "/react-admin/static/js/2.2ad31fcc.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/react-admin/static/js/2.2ad31fcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6fa8f48258aff8525d1",
+    "url": "/react-admin/static/js/main.2e575f61.chunk.js"
+  },
+  {
+    "revision": "6b82f940667522a583a4",
+    "url": "/react-admin/static/js/runtime-main.efed7abe.js"
+  }
+]);
